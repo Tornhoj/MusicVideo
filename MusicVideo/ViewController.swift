521b66bcde0eaec0813e5577f522752b22c82297
John Tornhøj <jtornh@gmail.com>
2016-03-25 17:11:39 +0100

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by John Tornhøj on 25/03/2016.
+//  Copyright © 2016 John Tornhøj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
